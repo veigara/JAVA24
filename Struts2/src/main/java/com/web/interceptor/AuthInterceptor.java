9ec8171abb3a04a221ca74501bdc1f41f9f65aa7
zhou <Administrator@LS--20171225UTQ>
2018-05-03 14:51:59 +0800

    2018-5-3 struts2  框架验证   标签   文件上传和下载

---
+++ Struts2/src/main/java/com/web/interceptor/AuthInterceptor.java
@@ -0,0 +1,37 @@
+package com.web.interceptor;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class AuthInterceptor extends AbstractInterceptor {
+
+	
+	/**
+	 * 拦截器方法
+	 */
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		
+		//获取session对象
+		Map<String,Object> session = ActionContext.getContext().getSession();
+		
+		//从session中获取数据
+		Object obj = session.get("admin");
+		
+		if(obj != null){
+			//已经登录
+			return invocation.invoke();
+		}else{
+			//没有登录
+			return Action.LOGIN;//"login"
+		}
+	}
+	
+	
+	
+
+}
